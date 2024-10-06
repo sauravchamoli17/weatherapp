e8aeeb822a58d00f4d377aebecf9a5ef225b75e0
Saurav Chamoli <sauravchamoli17@gmail.com>
2024-10-06 23:06:48 +0530

    Update generated apk

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { View, ImageBackground, StatusBar } from 'react-native';
-import { useSelector } from 'react-redux';
-import { RootState } from '../reducers';
+import { View, ImageBackground, StatusBar, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import { fetchWeather } from '../actions/weatherActions';
-import store from '../store';
 import { globalStyles } from '../styles/globalStyles';
 import Forecast from '../components/Forecast';
 
-const HomeScreen = ({ navigation }: any) => {
+const HomeScreen = () => {
   return (
     <View style={{ ...globalStyles.container }} className='relative'>
-      <StatusBar />
-      <ImageBackground source={require('../assets/bg.png')} style={{ ...globalStyles.bgImage }} blurRadius={70} resizeMode="cover">
-        <SearchBar />
-        <Forecast />
-      </ImageBackground>
+      <ScrollView>
+        <StatusBar />
+        <ImageBackground source={require('../assets/bg.png')} style={{ ...globalStyles.bgImage }} blurRadius={70} resizeMode="cover">
+          <SearchBar />
+          <Forecast />
+        </ImageBackground>
+      </ScrollView>
     </View>
   );
 };
